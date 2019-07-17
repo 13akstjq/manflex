a720aab1a441a43b394e66fa4ce0421af854e28a
13akstjq <13akstjq@naver.com>
2019-07-18 01:38:17 +0900

    setting project and add Router

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Router from "./Router";
+
+//만든 Router 사용
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Home from "../Routes/Home";
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
+
+// Route는 Router안에 있어야 함.
+export default () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/tv/popular" component={() => <div>popular</div>} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
+);
