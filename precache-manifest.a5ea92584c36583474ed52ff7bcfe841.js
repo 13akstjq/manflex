c5d23451425912d3ca30934d7cc89e0c8716b1db
13akstjq <13akstjq@naver.com>
2019-07-21 19:13:10 +0900

    Updates

---
+++ precache-manifest.a5ea92584c36583474ed52ff7bcfe841.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d38f0faa4b30f53965bddd71550960",
+    "url": "/manflex/index.html"
+  },
+  {
+    "revision": "abf61b068945ee8dfe58",
+    "url": "/manflex/static/js/2.232be154.chunk.js"
+  },
+  {
+    "revision": "b8f883b837a6e6a71656",
+    "url": "/manflex/static/js/main.6ae31488.chunk.js"
+  },
+  {
+    "revision": "626a5f7fe54aa7131b1a",
+    "url": "/manflex/static/js/runtime~main.8e50abbd.js"
+  }
+]);
